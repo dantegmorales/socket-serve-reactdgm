dc0962a42823e533c4ececda6e96e2df879102e2
itsdantemorales <dante.morales@itcglobals.com>
2021-07-09 11:30:37 -0500

    revert

--- models/sockets.js
+++ models/sockets.js
@@ -1,24 +1,31 @@
 
+
 class Sockets {
-    //constructor 
-    constructor(io){
+
+    constructor( io ) {
 
         this.io = io;
+
         this.socketEvents();
     }
-    //Metodo SocketEvents
-    socketEvents (){
-        //On conection 
-        this.io.on('connection', (socket) => {
-            //Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server',(data)=>{
-                console.log('data del cliente:',data);
-                //Emite el evento:mensaje-from-server
-                this.io.emit('mensaje-from-server',data);
+
+    socketEvents() {
+        // On connection
+        this.io.on('connection', ( socket ) => {
+
+            // Escuchar evento: mensaje-to-server
+            socket.on('mensaje-to-server', ( data ) => {
+                console.log( data );
+                
+                this.io.emit('mensaje-from-server', data );
             });
+            
+        
         });
     }
 
+
 }
+
 
 module.exports = Sockets;
